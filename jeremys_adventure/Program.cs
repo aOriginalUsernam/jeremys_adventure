287e8a25b99afce2a206609bb9c547864740233f
DennisAngel <1078087@hr.nl>
2024-02-12 16:03:13 +0100

    Update Program.cs

--- jeremys_adventure/Program.cs
+++ jeremys_adventure/Program.cs
@@ -4,7 +4,8 @@ internal class Program
     {
         Console.WriteLine("Hello, Jelle");
         Console.WriteLine("Hoi1");
-        Console.WriteLine("Did you know")
+        Console.WriteLine("Did you know");
+        Console.WriteLine("Do you know the wae");
     }
 }
 

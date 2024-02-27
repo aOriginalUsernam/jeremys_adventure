7860a838ec7dbe19bdb55641e72b12fb7c36704d
Perso0o0n <1075304@hr.nl>
2024-02-27 13:43:01 +0100

    added main gam loop

--- jeremys_adventure/Program.cs
+++ jeremys_adventure/Program.cs
@@ -2,11 +2,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Jelle");
-        Console.WriteLine("Hoi1");
-        Console.WriteLine("Did you know");
-        Console.WriteLine("Do you know the wae");
-        Monster mob = new("jelle", 1, [0, 1], 69);
+        Console.WriteLine("press enter to start...");
+        System.Console.ReadLine();
+        GameLoop.StartGame();
     }
 }
 

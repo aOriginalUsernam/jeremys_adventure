acbd7e2e2730a20fffed835db97a41921c8682c0
Perso0o0n <1075304@hr.nl>
2024-02-27 13:21:11 +0100

    adde ILevel to level2

--- jeremys_adventure/Level2.cs
+++ jeremys_adventure/Level2.cs
@@ -1,4 +1,10 @@
-public class level2
+public class level2 : ILevel
 {
-    
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int[] Pos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public bool StartLevel()
+    {
+        throw new NotImplementedException();
+    }
 }
